7315613a0a12bf98dbe07fdddb79c3ea4f8aea6f
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2019-01-24 18:36:52 +0100

    [TASK] Begin with version 3.0.0
    * add `composer.json`
    * raise version to `3.0.0-dev`

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,30 +10,25 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
-	'title' => 'My quiz and poll',
-	'description' => 'An very old, but advanced quiz-extension which can be used as a quiz, questionnaire, survey, poll, rating or psychological test.',
-	'category' => 'plugin',
-	'version' => '2.6.5',
-	'state' => 'stable',
-	'uploadfolder' => true,
-	'createDirs' => '',
-	'clearcacheonload' => false,
-	'author' => 'Kurt Gusbeth',
-	'author_email' => 'info@quizpalme.de',
-	'author_company' => '',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'typo3' => '6.2.0-7.6.99',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
-);
-
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'My quiz and poll',
+    'description' => 'A very old, but advanced quiz-extension which can be used as a quiz, questionnaire, survey, poll, rating or psychological test.',
+    'category' => 'plugin',
+    'version' => '3.0.0-dev',
+    'state' => 'stable',
+    'uploadfolder' => true,
+    'createDirs' => '',
+    'clearcacheonload' => false,
+    'author' => 'Kurt Gusbeth, Thorben Nissen',
+    'author_email' => 'info@quizpalme.de, thorben.nissen@kapp-hamburg.de',
+    'author_company' => '',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-9.5.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
